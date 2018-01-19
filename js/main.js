9e8981aa1e39a8af8cdc43692f434b3553e98bde
YSJ <s1003352@gmail.com>
2018-01-19 15:08:58 +0800

    畫面調整

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ $(".showBtn").click(function() {
     if($(".videoPart").css('display') == "none"){
     	$(".textPart").hide();
     	$(".videoPart").show();
+    	$(".showBtn").html("看字 &raquo;");
     } else {
     	$(".textPart").show();
     	$(".videoPart").hide();
+    	$(".showBtn").html("看影片 &raquo;");
     }
 });
