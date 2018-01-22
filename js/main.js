58bf02f907c5e807aaed1b025eb63d084e85f7fa
YSJ <s1003352@gmail.com>
2018-01-22 10:52:19 +0800

    畫面修改

--- js/main.js
+++ js/main.js
@@ -3,14 +3,12 @@ $(".showBtn").click(function() {
     if($(".videoPart").css('display') == "none"){
     	$(".textPart").hide();
     	$(".videoPart").show();
-    	$(".mail").css("height", "630px");
-    	$(".videoPart").css("height", "590px");
+        $(".mail").css("background", "#f9f9f9");
     	$(".showBtn").html("CLOSE");
     } else {
     	$(".textPart").show();
     	$(".videoPart").hide();
-    	$(".mail").css("height", "300px");
-    	// $(".textPart").css("height", "386px");
-    	$(".showBtn").html("TO: Alice");
+        $(".mail").css("background", "repeating-linear-gradient(45deg, #f0f0f0 10px, #f0f0f0 20px, #58c 20px, #58c 30px, #f0f0f0 30px, #f0f0f0 40px, #e55 40px, #e55 50px)");
+    	$(".showBtn").html("to: Alice");
     }
 });
