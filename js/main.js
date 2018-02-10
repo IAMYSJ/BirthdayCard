ddae1b5d6ab9c42c1f4e243b4b56ac6defa13f29
YSJ <s1003352@gmail.com>
2018-02-11 00:33:48 +0800

    畫面調整

--- js/main.js
+++ js/main.js
@@ -3,12 +3,10 @@ $(".showBtn").click(function() {
     if($(".videoPart").css('display') == "none"){
     	$(".textPart").hide();
     	$(".videoPart").show();
-        $(".mail").css("background", "#f9f9f9");
-    	$(".showBtn").html("CLOSE");
+    	$(".showBtn").html("Love you, YSJ 2018/02/16");
     } else {
     	$(".textPart").show();
     	$(".videoPart").hide();
-        $(".mail").css("background", "repeating-linear-gradient(45deg, #f0f0f0 10px, #f0f0f0 20px, #58c 20px, #58c 30px, #f0f0f0 30px, #f0f0f0 40px, #e55 40px, #e55 50px)");
-    	$(".showBtn").html("to: Alice");
+        $(".showBtn").html("TO: Alice");
     }
 });
